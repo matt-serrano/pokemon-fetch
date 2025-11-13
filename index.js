950b58fdf1e08a8e212e7d3ec52d6e2a58f429f9
matt-serrano <matt-serrano@users.noreply.github.com>
2025-11-12 19:31:18 -0500

    Project finished

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-fetch("https://pokeapi.co/api/v2/pokemon/pikachu");
+// fetch("https://pokeapi.co/api/v2/pokemon/pikachu")
+//     .then(response => {
+
+//         if (!response.ok) {
+//             throw new Error("Could not fetch resource");
+//         }
+//         return response.json();
+
+//     })
+//     .then(data => console.log(data.dataname))
+//     .error(error => console.error(error));
+
+
+async function fetchData() {
+    try {
+        const pokemonName = document.getElementById("pokemonName").ariaValueMax.toLowerCase();
+        const response = await fetch("https://pokeapi.co/api/v2/pokemon/${pokemonName}");
+
+        if (!response.ok) {
+            throw new error("Could not fetch resource");
+        }
+
+        const data = await response.json();
+        const pokemonSprite = data.sprites.front_default;
+        const imgElement = document.getElementById("pokemonSprite");
+
+        imgElement.src = pokemonSprite;
+        imgElement.style.display = "block";
+
+    } catch(error) {
+        console.error(error);
+    }   
+}
